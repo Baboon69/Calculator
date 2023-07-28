586f74c8b613498beab8bb0a1b8260af5657498c
Baboon69 <40399361+Baboon69@users.noreply.github.com>
2023-07-28 07:52:21 +0300

    add js support for number input and clear as placeholder for operations

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 let buttons;
-function Calculator(){
-    buttons=document.querySelectorAll(".button");
-    buttons.forEach((e) => {
-        e.addEventListener("click", () => {
-            console.log("you clicked a button!");
+let currentNumber, currentNumberStr;
+let display;
+
+function thisIsNumber(num){
+    let tmp=currentNumberStr+num;
+    currentNumber=Number(tmp);
+    currentNumberStr=`${currentNumber}`;
+    display.textContent=currentNumberStr;
+}
+function smthElse(){
+    console.log("u clicked smth else")
+}
+
+function isNumeric(x){
+    return !isNaN(x);
+}
+
+function Clear(){
+    currentNumber=0;
+    currentNumberStr="0";
+    display.textContent=currentNumberStr;
+}
+
+function AssignEventFunction(btn){
+    if(isNumeric(btn.textContent)){
+        btn.addEventListener("click", () => {
+            thisIsNumber(btn.textContent);
         });
-    });
+        return;
+    }
+    btn.addEventListener("click",Clear);
+    
+}
+
+
+
+
+
+function Calculator(){
+    currentNumber=0;
+    currentNumberStr="0";
+    display=document.querySelector("#display");
+    display.textContent=currentNumberStr;
+    buttons=document.querySelectorAll(".button");
+    buttons.forEach(
+        AssignEventFunction
+    )
     console.log(buttons);
 }
 
